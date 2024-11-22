4db6cfd5667998e482759051af76fde7fb9c994f
Jagadeshwaran P <148187521+jaga-Innovation@users.noreply.github.com>
2024-11-22 19:03:56 +0530

    Create Example.jsx

---
+++ src/Example.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Example() {
+    return (
+        <Fragment>
+            <h1>Hello</h1>
+            <p>This is a React Fragment example.</p>
+        </Fragment>
+    );
+}
+
+export default Example;
